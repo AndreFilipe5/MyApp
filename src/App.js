1092e3163acd0686af3f6088fb3f2a7bb6b31d05
Andre Santos <a.santos@dnctecnica.com>
2023-01-04 18:04:57 +0000

    first commit 2

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const[counter,setcounter]=useState(0)
+  function handleClickPlus(){
+    setcounter(counter+1)
+  }
+  function handleClickMinus(){
+    setcounter(counter-1)
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <hi>Cenas Maradas</hi>
+        <p>counter: {counter}</p>
+        {/* <p>setcounter: {setcounter}</p> */}
+        <button onClick={handleClickPlus}>+</button>
+        <button onClick={handleClickMinus}>-</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
